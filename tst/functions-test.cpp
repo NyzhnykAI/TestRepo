26776a242530506df044e83bc69f81a5b4dc7d7b
AINyzhnyk <AINyzhnyk@example.com>
2020-03-09 15:36:31 +0200

    Fixes

--- tst/functions-test.cpp
+++ tst/functions-test.cpp
@@ -20,7 +20,7 @@ TEST_F(AddTestTest, CheckDoubleAddition) {
     //arrange
     //act
     //assert
-    EXPECT_EQ(pointer->add(1.6),  5.0);
+    EXPECT_EQ(pointer->add(1.6),  6.0);
 }
 
 TEST_F(AddTestTest, CheckStringAddition) {
@@ -41,5 +41,5 @@ TEST_F(AddTestTest, CheckArrayAddition2) {
     //arrange
     //act
     //assert
-    EXPECT_EQ (pointer->getVarAr({3,4,5}), std::vector<int>({9,8,7,6,3,4,5}));
+    EXPECT_EQ (pointer->getVarAr({3,4,5}), std::vector<int>({9,8,7,6,5,4,3}));
 }
